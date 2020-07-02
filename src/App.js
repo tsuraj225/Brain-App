e5567170fdd3067cd7f021455c709b82f6453b44
suraj thallapelli <tsuraj225@gmail.com>
2020-07-02 15:15:53 +0530

    adding starting project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "tachyons";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <Rank />
+      {/*
+       <FaceRecognation />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Tilt from "react-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 250, width: 250 }}
+      >
+        <div className="Tilt-inner pa5">
+          <img style={{ paddingTop: "3px" }} src={brain} alt="brian logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+export default Logo;
